980783ce8d6d5491eecda3d34876e19d9be43fb2
jay.develops <swejesusperez@gmail.com>
2025-12-27 17:02:35 -0800

    feat: add decoding in utf8 from torrent file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import fs from "fs";
+import bencode from "bencode";
 
 const torrent = fs.readFileSync("puppy.torrent");
 const torrentString = torrent.toString("utf8");
 
 console.log(torrentString);
+
+// decode the data from bencode as torrent files use bencode opposed to json
+const data = Buffer.from(torrent);
+
+// bencode can decode as utf8 strings, removing utf8 arg converts to json
+const result = bencode.decode(data, "utf8");
+console.log(result);
